82f7ebc0cae3fe2468e30697b5fb22c958b9d212
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-08-16 11:39:36 +0800

    update

--- app/Http/Requests/Admin/Uploads/CreateUploadFileRequest.php
+++ app/Http/Requests/Admin/Uploads/CreateUploadFileRequest.php
@@ -11,7 +11,7 @@ class CreateUploadFileRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class CreateUploadFileRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'upload_folder_id'  => 'int|required',
+            'name'              => 'string|required|unique:upload_files',
+            'description'       => 'string|required',
+            'files'             => 'required|array'
         ];
     }
 }
--- app/Services/Admin/Uploads/UploadFilesService.php
+++ app/Services/Admin/Uploads/UploadFilesService.php
@@ -6,16 +6,24 @@ use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
 use App\Models\Uploads\UploadFile;
 use App\Repositories\Admin\Uploads\UploadFilesRepository;
+use App\Services\Admin\Uploads\UploadFoldersService;
 
 class UploadFilesService extends UploadFilesRepository
 {
-    public function __construct(UploadFile $model)
+    public function __construct(
+        UploadFile $model,
+        private readonly UploadFoldersService $uploadFoldersService
+    )
     {
         parent::__construct($model, ['upload_folder'], ['upload_folder']);
     }
 
     public function create($payload)
     {
+        $folderPath = $this->uploadFoldersService->getById($payload['upload_folder_id']);
+
+        return $folderPath;
+
         return parent::create($payload);
     }
 }
