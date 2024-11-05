371fc6d15b2cb8a08ed60194b7f3db8b484827fa
Patrick Noah Policarpio <patrickpolicarpio08@gmail.com>
2024-11-05 13:14:26 +0800

    updated upload file

--- database/migrations/2024_08_14_013648_create_upload_files_table.php
+++ database/migrations/2024_08_14_013648_create_upload_files_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('upload_files', function (Blueprint $table) {
             $table->id();
             $table->foreignId('upload_folder_id')->nullable()->constrained('upload_folders')->cascadeOnDelete();
-            $table->enum('type', ['incoming', 'outgoing']);
+            $table->enum('type', ['internal', 'external']);
             $table->string('date_received');
             $table->string('time_released');
             $table->string('date_letter');
